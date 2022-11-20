d90137b6f169a551881bf2b13aa55901f56dfb35
satabie <0426193@gmail.com>
2022-11-20 21:48:19 +0900

    changed routing

--- src/App.js
+++ src/App.js
@@ -12,9 +12,9 @@ function App() {
         <Header />
 
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/works" element={<MyWorks />} />
-          <Route path="/links" element={<Links />} />
+          <Route path="/my-portfolio" element={<HomePage />} />
+          <Route path="/my-portfolio/works" element={<MyWorks />} />
+          <Route path="/my-portfolio/links" element={<Links />} />
         </Routes>
       </div>
     </Router>
